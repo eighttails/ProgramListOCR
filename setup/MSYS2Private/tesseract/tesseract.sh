66a9a6052f84221eff1d6a19ee8c63c5d4e0a58c
Tadahito Yao <eighttails@nifty.com>
2018-11-10 13:22:07 +0900

    MSYS2Privateを更新

--- setup/MSYS2Private/tesseract/tesseract.sh
+++ setup/MSYS2Private/tesseract/tesseract.sh
@@ -25,15 +25,19 @@ echo "tesseract is already installed."
 exit 0
 fi
 
-#TESSERACT_VERSION=4.00.00alpha
-TESSERACT_SRC_DIR=tesseract-$BIT
+TESSERACT_VERSION=4.0.0
+TESSERACT_TAG=$TESSERACT_VERSION
+TESSERACT_ARCHIVE=tesseract-$TESSERACT_TAG.tar.gz
+TESSERACT_SRC_DIR=tesseract-$TESSERACT_VERSION
+TESSERACT_BUILD_DIR=$TESSERACT_SRC_DIR-$BIT
 
-if [ ! -e  $TESSERACT_SRC_DIR ]; then
-git clone https://github.com/tesseract-ocr/tesseract.git $TESSERACT_SRC_DIR
+if [ ! -e $TESSERACT_ARCHIVE ]; then
+wget -c https://github.com/tesseract-ocr/tesseract/archive/$TESSERACT_TAG/$TESSERACT_ARCHIVE
 fi
-
-pushd $TESSERACT_SRC_DIR
-git pull
+rm -rf $TESSERACT_SRC_DIR $TESSERACT_BUILD_DIR 
+tar xf $TESSERACT_ARCHIVE
+mv $TESSERACT_SRC_DIR $TESSERACT_BUILD_DIR
+pushd $TESSERACT_BUILD_DIR
 
 if [ -e Makefile ]; then
 make clean
