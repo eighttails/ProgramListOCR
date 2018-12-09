a2ebde371edd4dbd21e4fc0e90657dcb9d3b0675
Tadahito Yao <eighttails@nifty.com>
2018-12-09 22:38:25 +0900

    Windowsではcygpathを通してパスを決定するように戻した

--- fonts/downloadFonts.sh
+++ fonts/downloadFonts.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-SCRIPT_DIR=$(dirname $(readlink -f ${BASH_SOURCE:-$0}))
+if [ "$MINGW_CHOST" != "" ]; then
+    SCRIPT_DIR=$(cygpath -am $(dirname $(readlink -f ${BASH_SOURCE:-$0})))
+else
+    SCRIPT_DIR=$(dirname $(readlink -f ${BASH_SOURCE:-$0}))
+fi
 
 cd $SCRIPT_DIR
 
--- training/makeHexTessData.sh
+++ training/makeHexTessData.sh
@@ -8,7 +8,11 @@ else
 fi
 }
 
-SCRIPT_DIR=$(dirname $(readlink -f ${BASH_SOURCE:-$0}))
+if [ "$MINGW_CHOST" != "" ]; then
+    SCRIPT_DIR=$(cygpath -am $(dirname $(readlink -f ${BASH_SOURCE:-$0})))
+else
+    SCRIPT_DIR=$(dirname $(readlink -f ${BASH_SOURCE:-$0}))
+fi
 
 mkdir -p $SCRIPT_DIR/tessdata_out
 mkdir -p $SCRIPT_DIR/tessdata_tmp/hex
